9ff3f43e834873be4c02b70f10a46d825a09ad5f
Mahmoud Ibrahim <84087861+3wiida@users.noreply.github.com>
2024-03-12 07:00:49 +0200

    first commit

---
+++ NLP_Task_2.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer as p_stem
+from nltk.stem.snowball import SnowballStemmer as s_stem
+
+dataset_path = "Dataset/dataset.csv"
+porter_results_path = "Results/porter_output.csv"
+snowball_results_path = "Results/snowball_output.csv"
+
+def read_dataset(path):
+    return pd.read_csv(path)
+
+def tokenize_quote(quote):
+    return word_tokenize(quote)
+
+def stem_by_porter(words):
+    result = [p_stem().stem(word) for word in words]
+    return result
+
+def stem_by_snowball(words):
+    result = [s_stem(language="english").stem(word) for word in words]
+    return result
+
+
+def process():
+    data_frame = read_dataset(dataset_path)
+    data_frame["porter_stem_output"] = data_frame["original_text"].apply(tokenize_quote).apply(stem_by_porter)
+    data_frame["snowball_stem_output"] = data_frame["original_text"].apply(tokenize_quote).apply(stem_by_snowball)
+    print(data_frame["porter_stem_output"])
+    
+    data_frame["porter_stem_output"].to_csv(porter_results_path,index=False)
+    data_frame["snowball_stem_output"].to_csv(snowball_results_path,index=False)
+
+process()
